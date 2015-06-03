c73c498349ab9618ce978913579bec6f3a8f4a1b
david-borchers <dlb@st-andrews.ac.uk>
2015-06-03 13:15:32 +0100

    Added analytic p(x) calculation for Hayes & Buckland hazard rate model. Modified GoF functions a little.

--- R/GoF.r
+++ R/GoF.r
@@ -64,19 +64,32 @@ p.kolomogarov=function(x,inf=1000,dp=1e-4)
 #'@seealso \code{\link{fityx}} \code{\link{p.kolomogarov}}
 #'\code{\link{GoFx}}
 #'@export
-GoFy=function(fit,plot=FALSE){
+GoFy=function(fit,plot=FALSE,dotitle=FALSE){
   ystart=fit$ystart
   w=fit$w
   hr=match.fun(fit$hr)
+  if(fit$hr=="h1") analytic.F0=TRUE else analytic.F0=FALSE
   ystart=fit$ystart
   pi.x=match.fun(fit$pi.x)
+  b=fit$b
   logphi=fit$logphi
   
   x=fit$dat$x;y=fit$dat$y
   n=length(x)
-  Fy=(1-Sy(x=x,y=y,ymax=ystart,b=b,hfun=hr))
-  F0=(1-Sy(x=x,y=rep(0,length(y)),ymax=ystart,b=b,hfun=hr))
-  
+  if(analytic.F0) {
+    zeros=(y==0)
+    Fy=rep(NA,length(x))
+    if(length(zeros)>0){
+      Fy[zeros]=HBhr(x[zeros],h1.to.HB(b))
+      Fy[!zeros]=(1-Sy(x=x[!zeros],y=y[!zeros],ymax=ystart,b=b,hfun=hr))
+    }
+    F0=HBhr(x,h1.to.HB(b))
+#    F0=(1-Sy(x=x,y=rep(0,length(y)),ymax=ystart,b=b,hfun=hr))
+  } else {
+    Fy=(1-Sy(x=x,y=y,ymax=ystart,b=b,hfun=hr))
+    F0=(1-Sy(x=x,y=rep(0,length(y)),ymax=ystart,b=b,hfun=hr))
+  }
+    
   Fy0=Fy/F0
   Fy0.order=order(Fy0)
   yy=y[Fy0.order]
@@ -89,22 +102,70 @@ GoFy=function(fit,plot=FALSE){
   worst=which(abs(dF)==max(abs(dF))) # mark point on which Kolmogarov test hinges
   Dn=max(abs(dF))*sqrt(n)
   
-  p.cvm=nortest::cvm.test(Fy0)$p.value
+  p.cvm=goftest::cvm.test(Fy0)$p.value
   p.kolomogarov=1-p.kolomogarov(Dn)
   
   if(plot){
+    main=""
+    if(dotitle) main="Forward Dist. Q-Q Plot"
     plot(1-e.cdf,cdf,xlab="Empirical Distribution Function",
          ylab="Cumulative Distribution Function",
-         main="Forward Dist. Q-Q Plot",
+         main=main,
          xlim=c(0,1),ylim=c(0,1),pch="+")
     lines(c(0,1),c(1,0))
-    mtext(paste('p-values: Cramer-von Mises=',round(p.cvm,2),' ;
+    if(dotitle) mtext(paste('p-values: Cramer-von Mises=',round(p.cvm,2),' ;
                 kolomogarov=',round(p.kolomogarov,2)))
     points(1-e.cdf[worst],cdf[worst],col="red") # mark point on which Kolmogarov test hinges
   }
   
-  return(data.frame(p.cvm=p.cvm,D.kolomogarov=Dn,p.kolomogarov=p.kolomogarov))
+  pvals=c(p.cvm,p.kolomogarov);names(pvals)=c("Cramer-von Mises","Kolmogarov-Smirnov")
+  return(data.frame(pvals=pvals,D.kolomogarov=Dn))
 }
+
+
+#' @title Parameter conversion for Hayes+Buckland hazard rate model.
+#'
+#' @description
+#' Converts parameters from the 2-dimensional a*r^{-b} form of the Hayes and Buckland (1983) 
+#' hazard rate model used in \link{\code{h1}}, to parameters of the perpendicular distance form 
+#' of the model used in conventional distance sampling.
+#
+#' @param b parameter vector of the \link{\code{h1}}.
+#' @return
+#' parameter vector c(a1,b1) for Hayes and Buckland (1983) hazard rate model 
+#' g(x)=1-exp(-a1*x^(-(b1-1))).
+#' @seealso \code{\link{h1}}
+#' @examples
+#' h1.to.HB(c(-7, 1))
+h1.to.HB=function(b){
+  if(length(b)!=2) {
+    cat(b,"\n")
+    stop("b must be vector of length 2.")
+  }
+  b1=exp(b[2])
+  a=exp(b[1])
+  a1=a*gamma((b1-1)/2)*gamma(0.5)/(2*gamma(b1/2))
+  return(c(a1,b1))
+}
+
+#' @title Hayes+Buckland hazard rate model.
+#'
+#' @description
+#' Evaluates the Hayes and Buckland (1983) hazard rate model (HB model) as parameterised on 
+#' page 36 of that paper.
+#
+#' @param x perpendicular distance(s).
+#' @param theta parameter vector with theta[1] being HB model parameter a1 and theta[2] being b.
+#' @return
+#' g(x)=1-exp(-a1*x^(-(b-1))).
+#' @seealso \code{\link{h1.to.HB}}
+#' @examples
+#' xx=seq(0,0.03,length=100)
+#' p=HBhr(xx,h1.to.HB(c(-7, 0.85)))
+#' plot(xx,p,type="l",xlab="Perpendicular distance (x)",ylab="p(x)",ylim=c(0,1))
+HBhr=function(x,theta) 1-exp(-theta[1]*x^(-(theta[2]-1)))
+
+
 
 #' @title Goodness-of-fit in perpendicular dimension.
 #'
@@ -112,7 +173,7 @@ GoFy=function(fit,plot=FALSE){
 #' Calculates goodness-of-fit in perpendicular dimension, plots fit, and returns p-value and
 #' other stuff. Returns two p-values: \code{p.ks} is the Kolmogarov-Smirnov p-value (which is
 #' based on only the largest difference between emprical and theoretical cdfs), and 
-#' Cramer-von Misesp-value (which is based on all cdf values).
+#' Cramer-von Mises p-value (which is based on all cdf values).
 #
 #' @param hmltm fitted model, as output by \code{\link{est.hmltm}}
 #' @param plot If TRUE, does Q-Q plot. Point corresponding to largest difference between
@@ -126,7 +187,7 @@ GoFy=function(fit,plot=FALSE){
 #' \code{$qq.x} = empirical distribution function values.
 #'\code{$qq.y} = cumulative distribution function values.
 #'\code{$x} = x values.
-#'@depends nortest
+#'@depends goftest
 #'@seealso \code{\link{fityx}} \code{\link{p.kolomogarov}} \code{\link{GoFy}}
 #' @examples
 #' #'\dontrun{
@@ -144,12 +205,13 @@ GoFy=function(fit,plot=FALSE){
 #' rug(x=est.yx$dat$x)
 #' tst=GoFx(fit=est.yx,plot=TRUE)
 
-GoFx=function(fit,plot=FALSE,nint=100){
+GoFx=function(fit,plot=FALSE,nint=100,dotitle=FALSE){
   ystart=fit$ystart
   w=fit$w
   hr=match.fun(fit$hr)
   ystart=fit$ystart
   pi.x=match.fun(fit$pi.x)
+  b=fit$b
   logphi=fit$logphi
   
   x=fit$dat$x;y=fit$dat$y
@@ -173,18 +235,19 @@ GoFx=function(fit,plot=FALSE,nint=100){
   worst=which(abs(dF)==max(abs(dF)))
   Dn=max(abs(dF))*sqrt(n)
   p.ks=1-p.kolomogarov(Dn)
-  p.cvm=nortest::cvm.test(cdf)$p.value # Under model, cdf values are from uniform; default for cvm.test is "punif"
+  p.cvm=goftest::cvm.test(cdf)$p.value # Under model, cdf values are from uniform; default for cvm.test is "punif"
   if(plot) {
+    main=""
+    if(dotitle) main="Perp. Dist. Q-Q Plot"
     plot(edf,cdf,pch="+",xlim=c(0,1),ylim=c(0,1),
          xlab="Empirical Distribution Function",
-         ylab="Cumulative Distribution Function",main="Perp. Dist. Q-Q
-         Plot")
+         ylab="Cumulative Distribution Function",main=main)
     lines(c(0,1),c(0,1))
-    mtext(paste('p-values: Cramer-von Mises=',round(p.cvm,2),' ;
+    if(dotitle) mtext(paste('p-values: Cramer-von Mises=',round(p.cvm,2),' ;
                 kolomogarov=',round(p.ks,2)))
     points(edf[worst],cdf[worst],col="red")
   }
   
-  
-  return(list(p.cvm=p.cvm,D.kolomogarov=Dn,p.kolomogarov=p.ks,qq.x=edf,qq.y=cdf,x=x[cdf.order]))
+  pvals=c(p.cvm,p.ks);names(pvals)=c("Cramer-von Mises","Kolmogarov-Smirnov")
+  return(list(pvals=pvals,D.kolomogarov=Dn,qq.x=edf,qq.y=cdf,x=x[cdf.order]))
 }
