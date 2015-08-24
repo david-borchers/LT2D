306d9919561298d8107d64b91b9575f7dfe0396d
david-borchers <dlb@st-andrews.ac.uk>
2015-08-24 10:30:53 +0100

    Added primate and dolphin data objects + documentation

---
+++ R/data.r
@@ -0,0 +1,38 @@
+
+#' @name primate.dat
+#' @title Primate line transect survey data.
+#' @docType data
+#' @description Locations relative to the observer of 127 detections of primates from a 
+#' visual survey conducted by three sets of trained observers walking previously cut line 
+#' transects in primary tropical rainforest.
+#' @usage primate.dat
+#' @format A list with elements x (perpendicular distance) and y (forward distance).
+#' @source We are gerateful to Matthew Nowak from the Sumatran Orangutan Conservation 
+#' Programme (SOCP) for allowing us to use the primate survey data from the Jantho 
+#' Reintroduction Station. The initial survey was developed by Matthew Nowak and Serge 
+#' Wich (Liverpool John Moores University) and then undertaken by the SOCP with funding 
+#' from Chester Zoo. .
+#' @examples
+#'  data(primate.dat)
+NULL
+
+
+#' @name dolphin.dat
+#' @title Dolphin line transect survey data.
+#' @docType data
+#' @description Locations relative to the observer of 74 detections of dolphins from a 
+#' shipboard visual survey.
+#' @usage dolphin.dat
+#' @format A list with elements x (perpendicular distance) and y (forward distance).
+#' @source We are gerateful to North Atlantic Marine Mammal Commission (NAMMCO) and the 
+#' Faroese Museum of Natural History for allowing us to use the dolphin survey data from 
+#' 1995 North Atlantic Sightings Survey (NASS95). These data are analysed using 
+#' mark-recapture distance sampling methods by Ca\~{n}adas et al. (2004).
+#' @references 
+#' Ca\~{n}adas, A., Desportes, G. and Borchers, D.L. 2004. The estimation of the detection 
+#' function and g(0) for short-beaked common dolphins (Delphinis delphis), using 
+#' double-platform data collected during the NASS-95 Faroese survey. Journal of Cetacean 
+#' Research and Management 6: 191-198.
+#' @examples
+#'  data(primate.dat)
+NULL
